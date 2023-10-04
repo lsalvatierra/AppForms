4dcb70fac047e3418eedc74fa81269ff3a217c60
LuisAngel <luisangel.salvat@gmail.com>
2023-10-03 21:42:25 -0500

    formularios suma e IMC

--- app/src/main/java/pe/edu/idat/appforms/ImcActivity.java
+++ app/src/main/java/pe/edu/idat/appforms/ImcActivity.java
@@ -3,12 +3,52 @@ package pe.edu.idat.appforms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class ImcActivity extends AppCompatActivity {
+import pe.edu.idat.appforms.databinding.ActivityImcBinding;
+
+public class ImcActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityImcBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imc);
+        binding = ActivityImcBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btncalcularimc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Double talla = Double.parseDouble(binding.txttalla.getText().toString());
+        Double peso = Double.parseDouble(binding.txtpeso.getText().toString());
+        Double valorImc = calcularImc(talla, peso);
+        String resultado = diagnostico(valorImc);
+        binding.tvresultadoimc.setText("El valor IMC = "+ valorImc
+                + " su diágnostico es "+ resultado);
+    }
+
+    private Double calcularImc(Double talla, Double peso){
+        Double tallam = talla / 100;
+        return peso / (tallam * tallam);
+    }
+
+    private String diagnostico(Double valorImc){
+        String resultado = "";
+        if(valorImc <= 18.5){
+            resultado = "Por debajo del peso.";
+        }else if(valorImc <= 25){
+            resultado = "Con peso normal";
+        }else if (valorImc <= 30){
+            resultado = "Con sobrepeso";
+        }else if(valorImc <= 35){
+            resultado = "Obesidad Leve";
+        }else if(valorImc <= 39){
+            resultado = "Obesidad media";
+        }else{
+            resultado = "Obesidad mórbida";
+        }
+        return resultado;
     }
 }
--- app/src/main/java/pe/edu/idat/appforms/MainActivity.java
+++ app/src/main/java/pe/edu/idat/appforms/MainActivity.java
@@ -3,12 +3,32 @@ package pe.edu.idat.appforms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import pe.edu.idat.appforms.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btncalcular.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Integer numero1 = Integer.parseInt(binding.txtnumero1.getText().toString());
+        Integer numero2 = Integer.parseInt(binding.txtnumero2.getText().toString());
+        Integer suma = sumar(numero1, numero2);
+        binding.tvresultadosuma.setText("La suma es =" + suma);
+    }
+
+    private Integer sumar(Integer numero1, Integer numero2){
+        return numero1 + numero2;
     }
 }
